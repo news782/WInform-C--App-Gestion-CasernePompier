fe001bdb13c1270ade91078a4d55229600b31546
julot <julot424@gmail.com>
2025-05-26 10:54:24 +0200

    Ajout d'un gitIgnore pour vs

---
+++ UCmobilisations/UCmobilisations/UserControl1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace UCmobilisations
+{
+    partial class UCmobilisations
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.pnlEngins = new System.Windows.Forms.Panel();
+            this.lblEngins = new System.Windows.Forms.Label();
+            this.tblEngins = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlPompier = new System.Windows.Forms.Panel();
+            this.lblPompier = new System.Windows.Forms.Label();
+            this.tblPompier = new System.Windows.Forms.TableLayoutPanel();
+            this.btnFermer = new System.Windows.Forms.Button();
+            this.pnlEngins.SuspendLayout();
+            this.pnlPompier.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(18, 18);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(368, 25);
+            this.lblTitre.TabIndex = 0;
+            this.lblTitre.Text = "Mobilisation des engins et des pompiers";
+            this.lblTitre.Click += new System.EventHandler(this.lblTitre_Click);
+            this.lblTitre.MouseLeave += new System.EventHandler(this.lblTitre_MouseLeave);
+            this.lblTitre.MouseHover += new System.EventHandler(this.lblTitre_MouseHover);
+            this.lblTitre.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblTitre_MouseUp);
+            // 
+            // pnlEngins
+            // 
+            this.pnlEngins.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pnlEngins.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlEngins.Controls.Add(this.lblEngins);
+            this.pnlEngins.Controls.Add(this.tblEngins);
+            this.pnlEngins.Location = new System.Drawing.Point(23, 75);
+            this.pnlEngins.Name = "pnlEngins";
+            this.pnlEngins.Size = new System.Drawing.Size(260, 347);
+            this.pnlEngins.TabIndex = 1;
+            // 
+            // lblEngins
+            // 
+            this.lblEngins.AutoSize = true;
+            this.lblEngins.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEngins.Location = new System.Drawing.Point(52, 0);
+            this.lblEngins.Name = "lblEngins";
+            this.lblEngins.Size = new System.Drawing.Size(158, 25);
+            this.lblEngins.TabIndex = 1;
+            this.lblEngins.Text = "Engins mobilisés";
+            // 
+            // tblEngins
+            // 
+            this.tblEngins.AutoScroll = true;
+            this.tblEngins.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tblEngins.ColumnCount = 3;
+            this.tblEngins.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblEngins.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblEngins.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblEngins.Location = new System.Drawing.Point(10, 33);
+            this.tblEngins.Name = "tblEngins";
+            this.tblEngins.RowCount = 1;
+            this.tblEngins.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblEngins.Size = new System.Drawing.Size(237, 300);
+            this.tblEngins.TabIndex = 0;
+            // 
+            // pnlPompier
+            // 
+            this.pnlPompier.BackColor = System.Drawing.Color.White;
+            this.pnlPompier.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPompier.Controls.Add(this.lblPompier);
+            this.pnlPompier.Controls.Add(this.tblPompier);
+            this.pnlPompier.Location = new System.Drawing.Point(318, 75);
+            this.pnlPompier.Name = "pnlPompier";
+            this.pnlPompier.Size = new System.Drawing.Size(260, 347);
+            this.pnlPompier.TabIndex = 2;
+            // 
+            // lblPompier
+            // 
+            this.lblPompier.AutoSize = true;
+            this.lblPompier.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPompier.Location = new System.Drawing.Point(37, 0);
+            this.lblPompier.Name = "lblPompier";
+            this.lblPompier.Size = new System.Drawing.Size(182, 25);
+            this.lblPompier.TabIndex = 2;
+            this.lblPompier.Text = "Pompiers mobilisés";
+            // 
+            // tblPompier
+            // 
+            this.tblPompier.AutoScroll = true;
+            this.tblPompier.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tblPompier.ColumnCount = 3;
+            this.tblPompier.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPompier.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPompier.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPompier.Location = new System.Drawing.Point(11, 33);
+            this.tblPompier.Name = "tblPompier";
+            this.tblPompier.RowCount = 1;
+            this.tblPompier.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblPompier.Size = new System.Drawing.Size(237, 300);
+            this.tblPompier.TabIndex = 1;
+            // 
+            // btnFermer
+            // 
+            this.btnFermer.AutoSize = true;
+            this.btnFermer.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnFermer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFermer.ForeColor = System.Drawing.Color.Transparent;
+            this.btnFermer.Location = new System.Drawing.Point(445, 440);
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(133, 35);
+            this.btnFermer.TabIndex = 3;
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.UseVisualStyleBackColor = false;
+            this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
+            // 
+            // UCmobilisations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.Controls.Add(this.btnFermer);
+            this.Controls.Add(this.pnlPompier);
+            this.Controls.Add(this.pnlEngins);
+            this.Controls.Add(this.lblTitre);
+            this.Name = "UCmobilisations";
+            this.Size = new System.Drawing.Size(608, 485);
+            this.Load += new System.EventHandler(this.UCmobilisations_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UCmobilisations_MouseUp);
+            this.pnlEngins.ResumeLayout(false);
+            this.pnlEngins.PerformLayout();
+            this.pnlPompier.ResumeLayout(false);
+            this.pnlPompier.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Panel pnlEngins;
+        private System.Windows.Forms.Panel pnlPompier;
+        private System.Windows.Forms.Button btnFermer;
+        private System.Windows.Forms.TableLayoutPanel tblEngins;
+        private System.Windows.Forms.TableLayoutPanel tblPompier;
+        private System.Windows.Forms.Label lblEngins;
+        private System.Windows.Forms.Label lblPompier;
+    }
+}
---
+++ UCmobilisations/UCmobilisations/UserControl1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace UCmobilisations
+{
+    public partial class UCmobilisations: UserControl
+    {
+        DataTable dtEngins;
+        DataTable dtPompiers;
+        SQLiteConnection cx;
+
+        public UCmobilisations(DataTable datatableEngins, DataTable datatablePompier)
+        {
+            InitializeComponent();
+            dtEngins = datatableEngins;
+            dtPompiers = datatablePompier;
+        }
+
+        public UCmobilisations() //ce Constructeurs sera a supprimer ultérieurement 
+        {
+            InitializeComponent();
+
+
+            string chaine = "Data Source=SDIS67.db";
+
+            try
+            {
+                cx = new SQLiteConnection(chaine);
+                cx.Open();
+            }
+
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            cx.Close();
+
+            dtEngins = new DataTable();
+            dtPompiers = new DataTable();
+
+
+            string qry = @"select codeTypeEngin, nombre FROM Necessiter where idNatureSinistre = 9;";
+
+
+            
+            // /!\ NE PAS OUBLIER DES CHANGER LES REQUETES DANS L'AUTRE UC /!\
+
+            SQLiteCommand cmd = new SQLiteCommand(qry, cx);
+
+            SQLiteDataAdapter daEngins = new SQLiteDataAdapter(cmd);
+            daEngins.Fill(dtEngins);
+
+
+            //Remplissage d'une DataTable avec les Pompiers à affecter
+
+            qry = @"";
+
+
+            cmd = new SQLiteCommand(qry, cx);
+
+            SQLiteDataAdapter daPompiers = new SQLiteDataAdapter(cmd);  
+            daPompiers.Fill(dtPompiers);
+        }
+
+        private void UCmobilisations_Load(object sender, EventArgs e)
+        {
+            tblEngins.Controls.Clear();
+            tblEngins.RowStyles.Clear();
+            tblEngins.RowCount = 0;
+
+            
+            for(int i = 0;  i < dtEngins.Rows.Count;i++)
+            {
+                
+
+                tblEngins.RowCount++;
+                tblEngins.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+                //Ajout des images
+                PictureBox pic = new PictureBox();
+                pic.Image = Image.FromFile("Images/engin.png");
+                pic.SizeMode = PictureBoxSizeMode.Zoom;
+                pic.Width = 40;
+                pic.Height = 40;
+                tblEngins.Controls.Add(pic, 0, i);
+
+                Label lbl = new Label();
+                lbl.Font = new Font("Segoe UI", 12, FontStyle.Bold); 
+                lbl.AutoSize = true;
+                lbl.Text = dtEngins.Rows[i]["codeTypeEngin"].ToString();
+                lbl.Margin = new Padding(11);
+                tblEngins.Controls.Add(lbl, 1, i);
+
+            }
+
+        }
+
+        private void lblTitre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UCmobilisations_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void lblTitre_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void lblTitre_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void lblTitre_MouseLeave(object sender, EventArgs e)
+        {
+        }
+
+        private void btnFermer_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
